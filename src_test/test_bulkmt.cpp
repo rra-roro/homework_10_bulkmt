b5518660d46e764a71600ff4b8bb7d6489285da6
rra.roro@gmail.com <rra.roro@gmail.com>
2019-05-18 13:47:53 +0300

    have added two tests

--- src_test/test_bulkmt.cpp
+++ src_test/test_bulkmt.cpp
@@ -1,10 +1,17 @@
 ﻿#include "gtest/gtest.h"
 #include "gtest/gtest_prod.h"
 
+#include <string_view>
+#include <filesystem>
+
 #include "lib_version.h"
 
 #define PRIVATE_TEST 1
 #include "publisher.h"
+#include "bulk_reader.h"
+#include "queue.h"
+#include "thread_fn.h"
+#include "thread_mgr.h"
 
 TEST(version, test1)
 {
@@ -22,7 +29,7 @@ namespace roro_lib
       };
 
 
-      void subscriber_fn(){}
+      void subscriber_fn() {}
 
       struct subscriber_functor
       {
@@ -42,43 +49,43 @@ namespace roro_lib
       {
             publisher pbl;
 
-            pbl.subscribe(subscriber_fn); 
+            pbl.subscribe(subscriber_fn);
             ASSERT_TRUE(pbl.subscribers.size() == 1);
 
-            pbl.subscribe(subscriber_functor());                    // rvalue - добавляем
+            pbl.subscribe(subscriber_functor()); // rvalue - добавляем
             ASSERT_TRUE(pbl.subscribers.size() == 2);
 
             subscriber_functor sf;
 
-            pbl.subscribe(sf); 
+            pbl.subscribe(sf);
             ASSERT_TRUE(pbl.subscribers.size() == 3);
 
-            pbl.subscribe(sf, &subscriber_functor::test); 
+            pbl.subscribe(sf, &subscriber_functor::test);
             ASSERT_TRUE(pbl.subscribers.size() == 4);
 
             std::function<void(void)> fn1 = subscriber_fn;
             pbl.subscribe(fn1);
-            ASSERT_TRUE(pbl.subscribers.size() == 5); 
+            ASSERT_TRUE(pbl.subscribers.size() == 5);
 
-            pbl.subscribe(std::function<void(void)>(subscriber_fn));  //  rvalue - добавляем
+            pbl.subscribe(std::function<void(void)>(subscriber_fn)); //  rvalue - добавляем
             ASSERT_TRUE(pbl.subscribers.size() == 6);
 
 
             // ----
-            pbl.subscribe(subscriber_functor());                      //  rvalue - добавляем повторно
+            pbl.subscribe(subscriber_functor()); //  rvalue - добавляем повторно
             ASSERT_TRUE(pbl.subscribers.size() == 7);
 
             pbl.subscribe(std::function<void(void)>(subscriber_fn)); //   rvalue - добавляем повторно
             ASSERT_TRUE(pbl.subscribers.size() == 8);
 
             // ----
-            pbl.subscribe(subscriber_fn);                            // Уже есть в подписчиках
-            pbl.subscribe(sf);                                       // Уже есть в подписчиках
-            pbl.subscribe(sf, &subscriber_functor::test);            // Уже есть в подписчиках
+            pbl.subscribe(subscriber_fn);                 // Уже есть в подписчиках
+            pbl.subscribe(sf);                            // Уже есть в подписчиках
+            pbl.subscribe(sf, &subscriber_functor::test); // Уже есть в подписчиках
 
             std::function<void(void)> fn2;
-            pbl.subscribe(fn1);                 // function == nullptr  - игнорируем
-            pbl.subscribe(fn2);                 // Уже есть в подписчиках
+            pbl.subscribe(fn1); // function == nullptr  - игнорируем
+            pbl.subscribe(fn2); // Уже есть в подписчиках
 
             // ----
             auto l1 = []() { int i = 1; i++; };
@@ -87,7 +94,7 @@ namespace roro_lib
             ASSERT_TRUE(pbl.subscribers.size() == 9);
 
             pbl.subscribe(l1);
-            ASSERT_TRUE(pbl.subscribers.size() == 9);     // Уже есть в подписчиках
+            ASSERT_TRUE(pbl.subscribers.size() == 9); // Уже есть в подписчиках
 
             pbl.subscribe([]() { int i = 1; i++; }); //  rvalue
             ASSERT_TRUE(pbl.subscribers.size() == 10);
@@ -212,8 +219,8 @@ namespace roro_lib
       struct subscriber_state
       {
             int st = 0;
-            void test() { st+=1; };
-            void operator()() { st+=2; };
+            void test() { st += 1; };
+            void operator()() { st += 2; };
       };
 
       TEST_F(PublisherMixinTest, StoreSubscribersByRef)
@@ -359,7 +366,7 @@ namespace roro_lib
             using Base::notify;
             using Base::get_fn_by_handle;
             using Base::get_last_notify_exception;
-       
+
             using notify_ret_t = std::invoke_result_t<decltype(&publisher_not_void::notify), Base, int, int>;
 
         public:
@@ -372,7 +379,7 @@ namespace roro_lib
 
                   for (auto& retv : retvs)
                   {
-                        sum += retv.first;                        
+                        sum += retv.first;
                   }
                   return sum;
             };
@@ -385,7 +392,7 @@ namespace roro_lib
 
       int fn2(int a1, int a2)
       {
-            return a1 + a2 +1;
+            return a1 + a2 + 1;
       }
 
       int fn3(int a1, int a2)
@@ -412,5 +419,157 @@ namespace roro_lib
             ASSERT_TRUE(sum == 10);
       }
 
+      TEST(Command_Reader, ParserTest)
+      {
+            command_reader cmdr(3);
+
+            auto read = [&](std::string_view sv) {
+                  std::stringstream ss;
+                  ss << sv;
+                  cmdr.read(ss);
+            };
+
+            size_t index_calls = 0;
+
+            cmdr.subscribe([&](const std::vector<std::string>& command_list, std::time_t t) {
+                  switch (index_calls)
+                  {
+                  case 0:
+                        ASSERT_TRUE(command_list.size() == 3);
+                        ASSERT_TRUE(command_list[0] == "cmd1");
+                        ASSERT_TRUE(command_list[1] == "cmd2");
+                        ASSERT_TRUE(command_list[2] == "cmd3");
+                        break;
+                  case 1:
+                        ASSERT_TRUE(command_list.size() == 2);
+                        ASSERT_TRUE(command_list[0] == "cmd4");
+                        ASSERT_TRUE(command_list[1] == "cmd5");
+                        break;
+                  case 2:
+                        ASSERT_TRUE(command_list.size() == 4);
+                        ASSERT_TRUE(command_list[0] == "cmdA");
+                        ASSERT_TRUE(command_list[1] == "cmdB");
+                        ASSERT_TRUE(command_list[2] == "cmdC");
+                        ASSERT_TRUE(command_list[3] == "cmdD");
+                        break;
+                  case 3:
+                        ASSERT_TRUE(command_list.size() == 1);
+                        ASSERT_TRUE(command_list[0] == "cmd6");
+                        break;
+                  case 4:
+                        ASSERT_TRUE(command_list.size() == 6);
+                        ASSERT_TRUE(command_list[0] == "cmdA");
+                        ASSERT_TRUE(command_list[1] == "cmdB");
+                        ASSERT_TRUE(command_list[2] == "cmdAA");
+                        ASSERT_TRUE(command_list[3] == "cmdBB");
+                        ASSERT_TRUE(command_list[4] == "cmdC");
+                        ASSERT_TRUE(command_list[5] == "cmdD");
+                        break;
+                  case 5:
+                        ASSERT_TRUE(command_list.size() == 1);
+                        ASSERT_TRUE(command_list[0] == "cmd7");
+                        break;
+                  case 6:
+                        ASSERT_TRUE(command_list.size() == 3);
+                        ASSERT_TRUE(command_list[0] == "cmd11");
+                        ASSERT_TRUE(command_list[1] == "cmd22");
+                        ASSERT_TRUE(command_list[2] == "cmd33");
+                        break;
+                  case 7:
+                        ASSERT_FALSE(command_list.size() == 2);
+                        ASSERT_FALSE(command_list[0] == "cmdAN");
+                        ASSERT_FALSE(command_list[1] == "cmdBN");
+                        break;
+                  }
+                  index_calls++;
+            });
+
+            read("cmd1\ncmd2\ncmd3\n"
+                 "cmd4\ncmd5\n"
+                 "{\n"
+                 "cmdA\ncmdB\ncmdC\ncmdD\n"
+                 "}\n"
+                 "cmd6\n"
+                 "{\n"
+                 "cmdA\ncmdB\n{\ncmdAA\ncmdBB\n}\ncmdC\ncmdD\n"
+                 "}\n"
+                 "cmd7\n");
+
+            read("cmd11\ncmd22\ncmd33\n"
+                 "{\n"
+                 "cmdAN\ncmdBN\n");
+
+            bool is_exception = false;
+            try
+            {
+                  read("}\ncmdA\ncmdB\n");
+            }
+            catch (...)
+            {
+                  is_exception = true;
+            }
+            ASSERT_TRUE(is_exception);
+            ASSERT_TRUE(index_calls == 7);
+      }
+
+      TEST(Command_Reader, BulkFileSaveTest)
+      {
+            namespace fs = std::filesystem;
+
+            for (auto& p : fs::directory_iterator(fs::current_path()))
+            {
+                  if (p.is_regular_file() && p.path().extension() == ".log")
+                        fs::remove(p);
+            }
+            
+            using queue_tread_t = queue<std::vector<std::string>, std::time_t>;
+
+            command_reader cmdr(3);
+
+            auto read = [&](std::string_view sv) {
+                  std::stringstream ss;
+                  ss << sv;
+                  cmdr.read(ss);
+            };
+
+            queue_tread_t file_queue;
+            thread_mgr file_tmgr(2, file_queue, save_log_file(), &save_log_file::save);
+            cmdr.subscribe([&](const auto& vec, auto t) {
+                  try
+                  {
+                        file_queue.push_wait(vec, t);
+                  }
+                  catch (...)
+                  {
+                        std::throw_with_nested(std::runtime_error("'file' subscriber failed."));
+                  }
+            });
+
+            read("cmd1\ncmd2\ncmd3\n"
+                 "cmd4\ncmd5\n"
+                 "{\n"
+                 "cmdA\ncmdB\ncmdC\ncmdD\n"
+                 "}\n"
+                 "cmd6\n"
+                 "{\n"
+                 "cmdA\ncmdB\n{\ncmdAA\ncmdBB\n}\ncmdC\ncmdD\n"
+                 "}\n"
+                 "cmd7\n");
+
+            file_tmgr.finalize_threads();
+
+            size_t count_log_files = 0;
+
+            for (auto& p : fs::directory_iterator(fs::current_path()))
+            {
+                  if (p.is_regular_file() && p.path().extension() == ".log")
+                  {
+                        count_log_files++;
+                        fs::remove(p);
+                  }                  
+            }
+
+            ASSERT_TRUE(count_log_files == 6);
+      }
+
 }
- 
